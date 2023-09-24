772fbec5e1ad633a23865dcfcf734f1e284a2918
Swetan Reddy <swetanreddys@gmail.com>
2023-09-24 13:54:09 -0400

    modified : arm.py

--- arm.py
+++ arm.py
@@ -1,36 +1,33 @@
 import math
 
-def calculate_values(x, y, m, n, L1_constant, L2_constant):
-    R1 = math.sqrt(y**2 + (n - x)**2)
-    R2 = math.sqrt(y**2 + (n + x)**2)
-    
-    a1 = math.acos((L1_constant**2 + R1**2 - L2_constant**2) / (2 * L1_constant * R1))
-    a2 = math.acos((L1_constant**2 + R2**2 - L2_constant**2) / (2 * L1_constant * R2))
-    
-    b1 = math.acos((m**2 + R1**2 - R2**2) / (2 * m * R1))
-    b2 = math.acos((m**2 + R2**2 - R1**2) / (2 * m * R2))
-    
-    L1 = math.sqrt(R1**2 + m**2)
-    L2 = math.sqrt(R2**2 + m**2)
-    
-    theta1 = a1 + b1
-    theta2 = a2 + b2
-    
-    return R1, R2, L1, L2, a1, a2, b1, b2, theta1, theta2
+def calculate_values(x, y):
 
-# Constants
-m = 4
-n = 5
-L1_constant = 6
-L2_constant = 7
+    m = 120
+    n = 60
+    L1_constant = 120
+    L2_constant = 140
+
+    R1 = round(math.sqrt(y**2 + (n - x)**2), 2)
+    R2 = round(math.sqrt(y**2 + (n + x)**2), 2)
+    
+    a1 = round(math.degrees(math.acos((L1_constant**2 + R1**2 - L2_constant**2) / (2 * L1_constant * R1))), 2) 
+    a2 = round(math.degrees(math.acos((L1_constant**2 + R2**2 - L2_constant**2) / (2 * L1_constant * R2))), 2)
+    
+    b1 = round(math.degrees(math.acos((m**2 + R1**2 - R2**2) / (2 * m * R1))), 2)
+    b2 = round(math.degrees(math.acos((m**2 + R2**2 - R1**2) / (2 * m * R2))), 2)
+        
+    theta1 = round(a1 + b1, 2)
+    theta2 = round(a2 + b2, 2)
+    
+    return R1, R2, a1, a2, b1, b2, theta1, theta2
 
 # Example usage:
 x = 60
 y = 30
 
-R1, R2, L1, L2, a1, a2, b1, b2, theta1, theta2 = calculate_values(x, y, m, n, L1_constant, L2_constant)
+R1, R2, a1, a2, b1, b2, theta1, theta2 = calculate_values(x, y)
 
-print(f"R1: {R1}, R2: {R2}, L1: {L1}, L2: {L2}")
-print(f"a1: {math.degrees(a1)}, a2: {math.degrees(a2)}")
-print(f"b1: {math.degrees(b1)}, b2: {math.degrees(b2)}")
-print(f"theta1: {math.degrees(theta1)}, theta2: {math.degrees(theta2)}")
+# print(f"R1: {R1}, R2: {R2}")
+# print(f"a1: {a1}, a2: {a2}")
+# print(f"b1: {b1}, b2: {b2}")
+print(f"theta1: {theta1}, theta2: {theta2}")
