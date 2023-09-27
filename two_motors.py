859ef7a9b269b9afbaec3fcb70e31ebe78a88873
Swetan Reddy <swetanreddys@gmail.com>
2023-09-27 17:43:51 -0400

    modified: everything

--- two_motors.py
+++ two_motors.py
@@ -1,19 +1,24 @@
 from machine import Pin, I2C
 import time
 
+#motor-1 
 step_pin_motor1 = Pin(25, Pin.OUT)
 dir_pin_motor1 = Pin(26, Pin.OUT)
 
+#motor-2
+step_pin_motor2 = Pin(27, Pin.OUT)
+dir_pin_motor2 = Pin(14, Pin.OUT)
+
 steps_per_revolution = 200
 speed = 0.01
+
+
 direction_motor1 = 1
 degrees_to_rotate_motor1 = 90
 steps_to_rotate_motor1 = int((degrees_to_rotate_motor1 / 360) * steps_per_revolution)
 
 dir_pin_motor1.value(direction_motor1)
 
-step_pin_motor2 = Pin(27, Pin.OUT)
-dir_pin_motor2 = Pin(14, Pin.OUT)
 
 direction_motor2 = 0
 degrees_to_rotate_motor2 = 45
@@ -21,8 +26,8 @@ steps_to_rotate_motor2 = int((degrees_to_rotate_motor2 / 360) * steps_per_revolu
 
 dir_pin_motor2.value(direction_motor2)
 
-if steps_to_rotate_motor1 > steps_to_rotate_motor2:
 
+if steps_to_rotate_motor1 > steps_to_rotate_motor2:
     for i in range(steps_to_rotate_motor1):
         step_pin_motor1.on()
         time.sleep(speed)
@@ -38,7 +43,6 @@ if steps_to_rotate_motor1 > steps_to_rotate_motor2:
             time.sleep(speed)
 
 else:
-
     for i in range(steps_to_rotate_motor2):
         step_pin_motor2.on()
         time.sleep(speed)
@@ -52,8 +56,6 @@ else:
         else:
             step_pin_motor1.off()
             time.sleep(speed)
-
-
 
 
 step_pin_motor1.off()
